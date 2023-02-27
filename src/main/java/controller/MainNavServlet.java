3bb624d092959935de01ee593611e45666cff4c8
Andrew Steele <aesteele@dmacc.edu>
2023-02-26 19:34:07 -0600

    added an index.html page and a main navigation servlet, that will take the value from the radio buttons and direct the user to the selected choice. Just a skeleton now but all the if statements in place

---
+++ src/main/java/controller/MainNavServlet.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainNavServlet
+ */
+@WebServlet("/mainNavServlet")
+public class MainNavServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainNavServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String choice = request.getParameter("choice_selector");
+		
+		String path = "/index.html";
+		
+		if(choice.equals("add_venue"))
+		{
+			//path to page for adding a venue
+		}
+		else if(choice.equals("edit_venue"))
+		{
+			//path to page for editing a venue
+		}
+		else if(choice.equals("delete_venue"))
+		{
+			//path to page for deleting a venue
+		}
+		else if(choice.equals("add_concert"))
+		{
+			//path to page for adding a concert
+		}
+		else if(choice.equals("edit_concert"))
+		{
+			//path to page for editing a concert
+		}
+		else if(choice.equals("delete_concert"))
+		{
+			//path to page for deleting a concert
+		}
+		else if(choice.equals("view_venues"))
+		{
+			//path to page for viewing all the venues
+		}
+		else if(choice.equals("view_concerts"))
+		{
+			//path to page for viewing all the concerts
+		}
+		else if(choice.equals("concerts_by_venue"))
+		{
+			//path to page for adding a viewing concerts based on venue
+		}
+		
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
+
+}
